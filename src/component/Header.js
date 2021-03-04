b8e5263a534776cbb3030108804878ce984580d3
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2021-03-03 17:01:50 -0800

    building home section

--- src/component/Header.js
+++ src/component/Header.js
@@ -18,11 +18,28 @@ function Header () {
 		navMenu.classList.remove('show');
 	}
 
+	function scrollActive(){
+		const scrollY = window.pageYOffset
+
+		sections.forEach(current =>{
+			const sectionHeight = current.offsetHeight
+			const sectionTop = current.offsetTop - 50;
+			sectionId = current.getAttribute('id')
+
+			if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+				document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+			}else{
+				document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+			}
+		})
+	}
 
 
 	useEffect(() => {
 		showMenu('nav-toggle', 'nav-menu');
 		const navLinks = document.querySelectorAll('.nav__link');
+		const sections = document.querySelectorAll('section[id]');
+		window.addEventListener('scroll', scrollActive);
 		navLinks.forEach(n => n.addEventListener('click', linkAction));
 	})
 	
@@ -37,7 +54,7 @@ function Header () {
 
 				<div className="nav__menu" id="nav-menu">
 					<ul className="nav__list">
-						<li className="nav__item"><a className="nav__link" href="#Home">Home</a></li>
+						<li className="nav__item"><a className="nav__link active" href="#Home">Home</a></li>
 						<li className="nav__item"><a className="nav__link" href="#About">About</a></li>
 						<li className="nav__item"><a className="nav__link" href="#Skills">Skills</a></li>
 						<li className="nav__item"><a className="nav__link" href="#Projects">Projects</a></li>
