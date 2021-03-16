4b92c9f506827fb13b050bfa6bd36f19244871e7
Mohamed Habib Alouani <mhabib.alouani@gmail.com>
2021-03-16 20:47:02 +0100

    project section done

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-function Project () {
+function Project ({imgLink, link}) {
   return(
     <div className="portfolio__img">
-      <img src={require('../assets/madara.jpg').default} alt="pol" />
+      <img src={imgLink} alt="pol" />
         
       <div className="portfolio__link">
-        <a className="portfolio__link-name" href="#link1" > View Details </a>
+        <a className="portfolio__link-name" href={link} > View Details </a>
       </div>
     </div>
   )
--- src/component/sections/Projects.js
+++ src/component/sections/Projects.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Project from '../Project';
 
+
 function Projects () {
+  const [switchType, setSwitchType] = useState('Web');
+  const renderSwitch = (param) => {
+    switch (param) {
+      case 'Web':
+        return (<>
+                  <Project imgLink={require('../../assets/Webex1.png').default} link="#linkUIUX1" />
+                  <Project imgLink={require('../../assets/Webex2.png').default} link="#linkUIUX2" />
+                  <Project imgLink={require('../../assets/Webex3.png').default} link="#linkUIUX2" />
+                  <Project imgLink={require('../../assets/Webex4.png').default} link="#linkUIUX2" />
+                </>);
+      case 'Mobile':
+        return (<>
+                  <Project imgLink={require('../../assets/Uiex1.png').default} link="#linkUIUX1" />
+                  <Project imgLink={require('../../assets/Uiex2.png').default} link="#linkUIUX2" />
+                </>);
+      case 'UIUX':
+        return (<>
+                  <Project imgLink={require('../../assets/Uiex1.png').default} link="#linkUIUX1" />
+                  <Project imgLink={require('../../assets/Uiex2.png').default} link="#linkUIUX2" />
+                </>);
+      default:
+        return null
+    }
+  }
   return (
     <div className="portfolio section" id="portfolio">
       <h2 className="section-title">Projects</h2>
+      <div className="button__container">
+        <button 
+          className={switchType === 'Web' ? "button" : "button__light"}
+          title="School" 
+          onClick={() =>{setSwitchType('Web')}}
+        >Web</button>
+        <button 
+          className={switchType === 'Mobile' ? "button" : "button__light"}
+          title="School"
+          onClick={() =>{setSwitchType('Mobile')}}
+        >Mobile</button>
+        <button 
+          className={switchType === 'UIUX' ? "button" : "button__light"}
+          title="School"
+          onClick={() =>{setSwitchType('UIUX')}}
+        >UI/UX</button>
+      </div>
       <div className="portfolio__container bd-grid">
-        <Project />
-        <Project />
-        <Project />
+        {renderSwitch(switchType)}
       </div>
     </div>
   );
